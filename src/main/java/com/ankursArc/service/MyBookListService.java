b30ec1bcaa4a66008ef9b7bcd64c9c22ea84478b
Yash Prakash <sudonot.root@gmail.com>
2024-12-11 13:33:11 +0530

    initial commit, 4 errors

---
+++ src/main/java/com/ankursArc/service/MyBookListService.java
@@ -0,0 +1,28 @@
+package com.ankursArc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ankursArc.entity.MyBookList;
+import com.ankursArc.repository.MyBookRepository;
+
+@Service
+public class MyBookListService {
+	
+	@Autowired
+	private MyBookRepository mybook;
+	
+	public void saveMyBooks(MyBookList book) {
+		mybook.save(book);
+	}
+	
+	public List<MyBookList> getAllMyBooks(){
+		return mybook.findAll();
+	}
+	
+	public void deleteById(int id) {
+		mybook.deleteById(id);
+	}
+}
